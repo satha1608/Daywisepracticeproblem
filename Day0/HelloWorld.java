f1903e821a7e40dc8bdd879fb8fc7007cf0ae239
“satha1608” <sathasivam1608@gmail.com>
2023-01-21 14:53:25 +0530

    Day0 practice problem

---
+++ Day0/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		 while(true){
+			System.out.println("Sathasivam here ");
+			try{
+			    Thread.sleep(60000);
+			   }catch(Exception e){}
+		 }
+   }
+}
